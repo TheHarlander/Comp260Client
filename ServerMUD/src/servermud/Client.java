29bacc0675e170bbfb6ecc17803b7e2fc0d2dde4
Dean <theharlander260@gmail.com>
2017-04-18 15:43:24 +0100

    MultiThread Client chat server
    +bug where other clients can't see messages.

--- ServerMUD/src/servermud/Client.java
+++ ServerMUD/src/servermud/Client.java
@@ -7,23 +7,65 @@ package servermud;
  */
 import java.net.*;
 import java.io.*;
-
+import java.util.Scanner;
 public class Client {
     
     static Socket socket;
     static DataInputStream in;
+    static DataOutputStream out;
     
     public static void main(String[]args)throws Exception{
         System.out.println("Connecting..");
         socket = new Socket("localhost",7777);
         System.out.println("Connection succesful..");
         in = new DataInputStream(socket.getInputStream());
-        System.out.println("Recieving information..");
-        String test = in.readUTF();
-        System.out.println("Message from Server: " + test);
+        out = new DataOutputStream(socket.getOutputStream());
+        
+        Input input = new Input(in);
+        Thread thread = new Thread(input);
+        thread.start();
+        Scanner sc = new Scanner(System.in);  //gets the keystrokes and send it to server
+        System.out.println("Enter your name then press enter. ");
+        String name =sc.nextLine();
+        out.writeUTF(name);
+        while(true)
+        {
+            String sendMessage = sc.nextLine(); //everytime you type a line it takes it and send it to server
+            out.writeUTF(sendMessage);
+        
+        }
     }
     
     
     
     
+}
+
+
+class Input implements Runnable{
+    
+    DataInputStream in;
+
+    public Input(DataInputStream in){
+        this.in =in;
+        
+    
+    }
+    
+    
+    
+    public void run(){
+        while(true){
+            String message;
+            try{
+            message = in.readUTF();
+            System.out.println();
+            
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        
+        }    
+    }
 }
--- ServerMUD/src/servermud/Server.java
+++ ServerMUD/src/servermud/Server.java
@@ -7,26 +7,85 @@ package servermud;
  */
 import java.io.*;
 import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
  
-    
+    //DEFINITIONS
     static ServerSocket serverSocket;
     static Socket socket; 
     static DataOutputStream out;
-    
+    static Users[] user = new Users[10];
+    static DataInputStream in;
     
     public static void main(String[]args)throws Exception{
-        System.out.println("Start up server...");
-        serverSocket = new ServerSocket(7777);//creating a new server sockete(Connect to self, also could use 127.0.0.1)
+        System.out.println("Starting up server...");
+        serverSocket = new ServerSocket(7777);              //creating a new server sockete(Connect to self, also could use 127.0.0.1)
         System.out.println("Server started up..");
-        socket  = serverSocket.accept(); //our client will send request and server will decided to accpet and send it through socket.
+       
+        while(true){                                        //alows multiple clients to connect
+        socket  = serverSocket.accept();                    //our client will send request and server will decided to accpet and send it through socket.
+                                                            //we want to create a new thread everytime we get a connection
+        for(int i=0;i<10;i++){
         System.out.println("Connection from"+ socket.getInetAddress());
         out = new DataOutputStream(socket.getOutputStream());
-        out.writeUTF("This is a test socket");
-        System.out.println("Data has been sent");
+        in = new DataInputStream(socket.getInputStream());
+        
+        if (user[i]==null){                                 //if the first one is null create a new thread if not move on
+                user[i] = new Users(out,in,user);
+            Thread thread = new Thread(user[i]);
+            thread.start();
+            break;
+        
+        
+                           }
+        }
+        }
+    }
+}
+
+class Users implements Runnable{
+    DataOutputStream out;
+    DataInputStream in;
+    Users[] user = new Users[10];
+    String name;
+//everytime we get an input we wanna send the output to every client
+    public Users(DataOutputStream out, DataInputStream in,Users[] user){
+        
+        this.out = out;
+        this.in = in;
+        this.user = user;
         
     }
+   
     
+    public void run(){
+        
+        try {
+            name = in.readUTF();
+        } catch (IOException ex) {
+            Logger.getLogger(Users.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        while(true){
+            try {
+                //if we get a message form the client we want to send that message to all the users.
+                String message = in.readUTF();
+                for(int i=0; i <10; i++){
+                if (user[i] != null){                      //if user i exists send user i message
+                    user[i].out.writeUTF(name +": " +message);
+                
+                }       
+                
+                }
+                
+            } catch (IOException e) {
+                this.out = null;
+                this.in = null;
+            }
+            
+            
+            }   
     
+        }
 }
