09378d6e14a51fdf5570f77a1292dae7c51c18a2
Dean <theharlander260@gmail.com>
2017-04-18 15:06:40 +0100

    Basic Server Clinet implmeneted
    +Server sockets and client  set up
    +able to connect and send and recieven emssage

--- ServerMUD/src/servermud/Client.java
+++ ServerMUD/src/servermud/Client.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package servermud;
 
 /**
  *
  * @author Dean
  */
+import java.net.*;
+import java.io.*;
+
 public class Client {
+    
+    static Socket socket;
+    static DataInputStream in;
+    
+    public static void main(String[]args)throws Exception{
+        System.out.println("Connecting..");
+        socket = new Socket("localhost",7777);
+        System.out.println("Connection succesful..");
+        in = new DataInputStream(socket.getInputStream());
+        System.out.println("Recieving information..");
+        String test = in.readUTF();
+        System.out.println("Message from Server: " + test);
+    }
+    
+    
+    
     
 }
--- ServerMUD/src/servermud/Server.java
+++ ServerMUD/src/servermud/Server.java
@@ -5,6 +5,28 @@ package servermud;
  *
  * @author Dean
  */
+import java.io.*;
+import java.net.*;
+
 public class Server {
+ 
+    
+    static ServerSocket serverSocket;
+    static Socket socket; 
+    static DataOutputStream out;
+    
+    
+    public static void main(String[]args)throws Exception{
+        System.out.println("Start up server...");
+        serverSocket = new ServerSocket(7777);//creating a new server sockete(Connect to self, also could use 127.0.0.1)
+        System.out.println("Server started up..");
+        socket  = serverSocket.accept(); //our client will send request and server will decided to accpet and send it through socket.
+        System.out.println("Connection from"+ socket.getInetAddress());
+        out = new DataOutputStream(socket.getOutputStream());
+        out.writeUTF("This is a test socket");
+        System.out.println("Data has been sent");
+        
+    }
+    
     
 }
